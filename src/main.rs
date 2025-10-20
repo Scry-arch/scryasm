6d876fc2d0c88b2c3a58147279ce13114664652c
Emad Jacob Maroun <emoun.open@gmail.com>
2025-10-20 17:20:01 +0200

    Initial support for assembling text to machine code.

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+#![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
+
+use clap::Parser;
+use scry_asm::Assemble;
+use std::io;
+use std::io::{stdout, Write};
+
+#[derive(clap::ValueEnum, Clone, Eq, PartialEq)]
+enum OutputFormat
+{
+    /// Each output byte is printed as a two-digit hexadecimal
+    TextHex,
+    
+    /// Output directly
+    Raw,
+}
+
+/// Command-line arguments
+#[derive(Parser)]
+struct Cli
+{
+    /// Disassemble machine code to textual assembly
+    #[clap(short, long)]
+    disassemble: bool,
+    
+    #[clap(long)]
+    #[arg(value_enum, default_value_t = OutputFormat::TextHex)]
+    out_format: OutputFormat,
+}
+
+fn main() -> io::Result<()> {
+    let args = Cli::parse();
+    
+    let mut stdin_buf = String::new();
+    io::stdin().read_line(&mut stdin_buf)?;
+    
+    // File is in textual assembly, assemble it
+    let assembled = scry_asm::Raw::assemble(std::iter::once(
+        stdin_buf.as_str(),
+    ))
+        .unwrap();
+    
+    if args.out_format == OutputFormat::TextHex {
+        let mut iter = assembled.iter();
+        print!("{:02x}", iter.next().unwrap());
+        iter.for_each(|b| print!(" {:02x}", b));
+    } else {
+        stdout().write(assembled.as_slice()).unwrap();
+    };
+    Ok(())
+}
---
+++ tests/tests.rs
@@ -0,0 +1,85 @@
+use std::fmt::Write;
+use assert_cmd::assert::{Assert, AssertError};
+use assert_cmd::Command;
+use predicates::prelude::predicate;
+use quickcheck_macros::quickcheck;
+use scry_isa::Instruction;
+use scry_isa::Parser;
+
+/// Where temporary test files should be created.
+pub const TEMPORARY_DIR: &'static str = "tests/tmp";
+
+/// Executes the program with the given input on stdin, returning a testable assert.
+fn invoke_assemble<S: Into<Vec<u8>>>(input: S, args: &[&str]) -> Assert {
+	let mut cmd = Command::cargo_bin("scryasm").unwrap();
+	
+	cmd.write_stdin(input);
+	cmd.args(args);
+	
+	cmd.assert()
+}
+
+fn to_encoded(instructions: &Vec<Instruction>) -> Vec<u8> {
+	instructions.iter().fold(Vec::new(), |mut v, b| {v.extend_from_slice(&b.encode().to_le_bytes()); v})
+}
+
+/// Returns the textual assembly of the given instruction sequence
+fn to_text_assembly(instructions: &Vec<Instruction>) -> String {
+	let mut asm_buf = String::new();
+	for instr in instructions {
+		Instruction::print(&instr, &mut asm_buf).unwrap();
+		asm_buf.write_char(' ').unwrap();
+	}
+	asm_buf
+}
+
+/// Tests can get instructions in textual form as input through stdin
+/// and output its hexadecimal encoding
+#[quickcheck]
+fn text_instruction_to_text_hex(first: Instruction, mut instructions: Vec<Instruction>) -> Result<(), AssertError> {
+	// Ensure there is at least 1 instruction
+	instructions.push(first);
+	
+	let encoded = to_encoded(&instructions);
+	
+	// Convert encoding to textual hex
+	let expected_text_hex = encoded.iter().fold(String::new(), |mut s, b| {
+		write!(&mut s, "{:02x} ", b).unwrap();
+		s
+	});
+	
+	let asm_buf = to_text_assembly(&instructions);
+	
+	// Assemble
+	let assert = invoke_assemble(asm_buf,&[]);
+	
+	// Test
+	assert
+		.try_code(predicate::eq(0))?
+		.try_stdout(predicate::eq(expected_text_hex.trim()))?
+		.try_stderr(predicates::str::is_empty())
+		.map(|_| ())
+}
+
+/// Tests can get instructions in textual form as input through stdin
+/// and output it raw
+#[quickcheck]
+fn text_instruction_to_raw(first: Instruction, mut instructions: Vec<Instruction>) -> Result<(), AssertError> {
+	// Ensure there is at least 1 instruction
+	instructions.push(first);
+	
+	// Get instruction encoding
+	let encoded = to_encoded(&instructions);
+	
+	let asm_buf = to_text_assembly(&instructions);
+	
+	// Assemble
+	let assert = invoke_assemble(asm_buf,&["--out-format=raw"]);
+	
+	// Test
+	assert
+		.try_code(predicate::eq(0))?
+		.try_stdout(predicate::eq(encoded))?
+		.try_stderr(predicates::str::is_empty())
+		.map(|_| ())
+}
